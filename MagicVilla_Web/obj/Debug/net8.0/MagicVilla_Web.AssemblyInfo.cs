952fc3da524f1221cbdc61df4c531757a15d8e2a
mustafas3aad <mostafasaadtornado7@gmail.com>
2025-09-24 23:04:10 +0300

    Add ScreenShoot

--- MagicVilla_Web/obj/Debug/net8.0/MagicVilla_Web.AssemblyInfo.cs
+++ MagicVilla_Web/obj/Debug/net8.0/MagicVilla_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2150bcddf2908eeb1e9d3f87558d1cd587d2fdc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec0872a8a552343776da1e9cde9181e3af07ece7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
